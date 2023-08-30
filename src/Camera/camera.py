abbbcd7f47cf9ddbf32c1dddee524933af2fe9be
Eden Cohen <eededeedencohen@gmail.com>
2023-08-30 12:08:45 +0300

    delete not necessary methods and change the print error to log

--- src/Camera/camera.py
+++ src/Camera/camera.py
@@ -1,7 +1,8 @@
 import cv2
 import threading
-import time
 import numpy as np
+import logging
+from typing import Union
 
 
 class Camera:
@@ -32,16 +33,15 @@ class Camera:
         if self.cap is None:
             self.cap = cv2.VideoCapture(self.camera_id)
 
-            # Check if the camera opened successfully
             if not self.cap.isOpened():
-                print(f"Error: Camera with ID {self.camera_id} not found or is being used by another application!")
+                logging.error(f"Camera with ID {self.camera_id} not found or is being used by another application!")
                 self.cap = None
                 return
             else:
-                print("Camera successfully initialized!")
+                logging.info("Camera successfully initialized!")
                 self.is_streaming = True
         else:
-            print("Camera is already streaming!")
+            logging.info("Camera is already streaming!")
 
     def showLive(self) -> None:
         """
@@ -49,7 +49,7 @@ class Camera:
             display the live feed from the camera
         """
         if not self.is_streaming:
-            print("Error: Camera is not streaming!")
+            logging.error("Camera is not streaming!")
             return
 
         def display_feed():
@@ -60,7 +60,7 @@ class Camera:
             while self.is_streaming:
                 is_frame_available, frame = self.cap.read()
                 if not is_frame_available:
-                    print("Error: Can't receive frame!")
+                    logging.error("Error: Can't receive frame!")
                     break
                 cv2.imshow('Live Camera Feed', frame)
                 if cv2.waitKey(1) == ord('q'):
@@ -70,25 +70,7 @@ class Camera:
 
         threading.Thread(target=display_feed).start()
 
-    def SaveCurrentFrame(self, path: str) -> None:
-        """
-        @summary:
-            save the current frame to the given path.
-            the name of the file will be frame_<current_time>.jpg
-        """
-        if not self.is_streaming:
-            print("Error: Camera is not streaming!")
-            return
-
-        ret, frame = self.cap.read()
-        if not ret:
-            print("Error: Can't receive frame!")
-            return
-
-        name = path + f'frame_{time.time()}.jpg'
-        cv2.imwrite(name, frame)
-
-    def getCurrentFrame(self) -> np.ndarray:
+    def getCurrentFrame(self) -> Union[np.ndarray, None]:
         """
         @summary: int
             get the current frame from the camera
@@ -96,17 +78,13 @@ class Camera:
             the current frame from the camera
         """
         if not self.is_streaming:
-            print("Error: Camera is not streaming!")
-            return np.array([])  # Return an empty numpy array for consistency
+            logging.error("Camera is not streaming!")
+            return None
 
         is_frame_available, frame = self.cap.read()
         if not is_frame_available:
-            print("Error: Can't receive frame!")
-            return np.array([])  # Return an empty numpy array for consistency
+            logging.error("Error: Can't receive frame!")
+            return None
 
         return frame
-
-    def showFrame(self, frame: np.ndarray) -> None:
-        cv2.imshow('Displayed Frame', frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    
